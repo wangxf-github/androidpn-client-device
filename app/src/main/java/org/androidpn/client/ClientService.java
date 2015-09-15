76930f8a79eafb36d4e9b397de5d573053c03148
Saber <qingduan@live.com>
2015-09-15 20:52:36 +0800

    test+离线定位+不死服务

---
+++ app/src/main/java/org/androidpn/client/ClientService.java
@@ -0,0 +1,40 @@
+package org.androidpn.client;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.zs.devicemanager.R;
+
+/**
+ * Created by Saber on 2015/9/15.
+ */
+public class ClientService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        flags = START_STICKY;
+        Log.e("clientService","onStart......");
+        ServiceManager serviceManager = new ServiceManager(this);
+        serviceManager.setNotificationIcon(R.drawable.notification);
+        serviceManager.startService();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Intent localIntent = new Intent();
+        localIntent.setClass(this, ClientService.class); //销毁时重新启动Service
+        this.startService(localIntent);
+    }
+}
